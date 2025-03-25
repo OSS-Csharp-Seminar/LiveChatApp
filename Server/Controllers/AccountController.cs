84471e238426282afae1ad78197436f05c84d000
StrukanMarko <strukanmarko03@gmail.com>
2025-03-25 21:50:20 +0100

    Add files via upload

---
+++ Server/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using ChatApp.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ChatContext _chatContext;
+
+        public AccountController(ChatContext chatContext)
+        {
+            _chatContext = chatContext;
+        }
+
+        public async Task<IActionResult> Register(RegisterDTO dto, CancellationToken cancellationToken)
+        {
+
+        }
+
+    }
+}
---
+++ Server/Data/ChatContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatApp.Server.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Server.Data
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions<ChatContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
